17ea0c96d512f731422f33d2fe95bc2065d6dfa5
Dada K <dadak@Dadas-MacBook-Air.local>
2022-09-29 11:21:10 +0900

    modal창 띄우기까지 완료

--- Mini_project/get-user-info/src/components/Users/AddUser.jsx
+++ Mini_project/get-user-info/src/components/Users/AddUser.jsx
@@ -3,10 +3,12 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import classes from './AddUser.module.css';
+import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = props => {
   const [enteredUsername, setEnteredUsername] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
+  const [error, setError] = useState();
 
   const changeUsername = e => {
     setEnteredUsername(e.target.value);
@@ -18,23 +20,43 @@ const AddUser = props => {
 
   const AddUserInfo = e => {
     e.preventDefault();
-    if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) return;
-    if(enteredAge < 1) return;
+    if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+      setError({
+        title : 'Invalid input',
+        message: 'Please enter a valid name and age (non-empty values).',
+      })
+      return;
+    } 
+    if(enteredAge < 1){
+      setError({
+        title : 'Invalid age',
+        message: 'Please enter a valid age (> 0).',
+      })
+      return;
+    };
+
     props.onAddUserList(enteredUsername, enteredAge);
     setEnteredUsername('');
     setEnteredAge('');
   }
 
+  const errorHandler = () => {
+    setError(null)
+  }
+
   return (
-    <Card className={classes.input}>
-      <form onSubmit={AddUserInfo}>
-        <label htmlFor='username'>User</label>
-        <input id='username' type='text' onChange={changeUsername} value={enteredUsername}/>
-        <label htmlFor='age'>Age</label>
-        <input id='age' type='number' onChange={changeAge} value={enteredAge}/>
-        <Button type='submit'>Add User</Button>
-      </form>
-    </Card>
+    <div>
+      {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/> }
+      <Card className={classes.input}>
+        <form onSubmit={AddUserInfo}>
+          <label htmlFor='username'>User</label>
+          <input id='username' type='text' onChange={changeUsername} value={enteredUsername}/>
+          <label htmlFor='age'>Age</label>
+          <input id='age' type='number' onChange={changeAge} value={enteredAge}/>
+          <Button type='submit'>Add User</Button>
+        </form>
+      </Card>
+    </div>
 
   )
 }
--- Mini_project/get-user-info/src/components/Users/UserList.jsx
+++ Mini_project/get-user-info/src/components/Users/UserList.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Card from '../UI/Card'
+import classes from './UsersList.module.css';
 
 const UserList = props => {
   return (
     <Card>
-      <ul>
-      {props.users.map(user => <li key={user.id}>{user.username} ({user.age} years old)</li>)}
-    </ul>
+      <ul className={classes.users}>
+       {props.users.map(user => 
+         <li key={user.id}>
+          {user.username} ({user.age} years old)
+         </li>)}
+      </ul>
     </Card>
   )
 };
