2b29864e00137c3d707b91a1f724998c69a3e69e
Dada K <dadak@Dadas-MacBook-Air.local>
2022-09-29 16:47:02 +0900

    try to use potals with ErrorModal components and state management  has changed from useState to ref

---
+++ Mini_project/potalAndref/src/components/Users/AddUser.js
@@ -0,0 +1,71 @@
+import React, { useState, useRef } from 'react';
+
+import Card from '../UI/Card';
+import Button from '../UI/Button';
+import ErrorModal from '../UI/ErrorModal';
+import classes from './AddUser.module.css';
+import Wrapper from '../Helpers/Wrapper';
+
+const AddUser = (props) => {
+  const [error, setError] = useState();
+
+  const userInputRef = useRef();
+  const ageInputRef = useRef();
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    const userName = userInputRef.current.value;
+    const userAge = ageInputRef.current.value;
+    if (userName.trim().length === 0 || userAge.trim().length === 0) {
+      setError({
+        title: 'Invalid input',
+        message: 'Please enter a valid name and age (non-empty values).',
+      });
+      return;
+    }
+    if (+userAge < 1) {
+      setError({
+        title: 'Invalid age',
+        message: 'Please enter a valid age (> 0).',
+      });
+      return;
+    }
+    props.onAddUser(userName, userAge);
+
+  };
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <Wrapper>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            ref={userInputRef}
+          />
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            id="age"
+            type="number"
+            ref={ageInputRef}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </Wrapper>
+  );
+};
+
+export default AddUser;
