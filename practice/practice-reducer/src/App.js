cc5ddd4e63f36388866aa1e26bd276f3e2a9c126
Dada K <dadak@Dadas-MacBook-Air.local>
2022-10-01 16:01:53 +0900

    check the state of login at the first time with useEffect

--- practice/practice-reducer/src/App.js
+++ practice/practice-reducer/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -6,6 +6,14 @@ import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+
+  //로그인 했는지 상태 가져오기, 만약에 로그인 상태면 로그인상태 true로 전환
+  useEffect(() => {
+    const isLoggedInInfo = localStorage.getItem('isLoggedIn');
+    if(isLoggedIn === '1') setIsLoggedIn(true);
+  }, [])
+  
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
