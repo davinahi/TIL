95447364a401d6b074f50b325ac93781e7bff086
Dada K <dadak@Dadas-MacBook-Air.local>
2022-10-12 11:12:56 +0900

    :memo: recursion - Mimic Math.pow()

--- Algorithm/recursion.js
+++ Algorithm/recursion.js
@@ -1,1 +1,9 @@
+//Mimic the functionality of Maht.pow() with the recursion
+function power(base, exponent) {
+  if (exponent === 0) return 1;
+  return base * power(base, exponent - 1);
+}
 
+console.log(power(2, 2));
+console.log(power(6, 3));
+console.log(power(7, 0));
