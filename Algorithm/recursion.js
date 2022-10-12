76fc1487c29e9cf3ee686765ab94eec609aae868
Dada K <dadak@Dadas-MacBook-Air.local>
2022-10-12 11:36:49 +0900

    :memo: recursion - array stuff2

--- Algorithm/recursion.js
+++ Algorithm/recursion.js
@@ -1,18 +1,18 @@
-// //Mimic the functionality of Maht.pow() with the recursion
-// function power(base, exponent) {
-//   if (exponent === 0) return 1;
-//   return base * power(base, exponent - 1);
-// }
+//Mimic the functionality of Maht.pow() with the recursion
+function power(base, exponent) {
+  if (exponent === 0) return 1;
+  return base * power(base, exponent - 1);
+}
 
-// console.log(power(2, 2));
-// console.log(power(6, 3));
-// console.log(power(7, 0));
+console.log(power(2, 2));
+console.log(power(6, 3));
+console.log(power(7, 0));
 
-// //factorial
-// function factorial(num) {
-//   if (num === 0) return 1;
-//   return num * factorial(num - 1);
-// }
+//factorial
+function factorial(num) {
+  if (num === 0) return 1;
+  return num * factorial(num - 1);
+}
 
 //Returns the product of them all which takes in an array
 const productOfArray = (arr) => {
