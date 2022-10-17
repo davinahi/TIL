ff9aebbe18f8bace54d157ebef72888773765773
Dada K <dadak@Dadas-MacBook-Air.local>
2022-10-17 11:42:25 +0900

    recursion - capitalize

--- Algorithm/recursion(medium).js
+++ Algorithm/recursion(medium).js
@@ -6,3 +6,13 @@ function reverse(s) {
 }
 
 console.log(reverse("awesome"));
+
+//capitalize**********************************************************************
+//Zeze's code
+function capitalizeFirst(arr) {
+  if (!arr.length) return [];
+  arr[0] = arr[0].replace(arr[0][0], arr[0][0].toUpperCase());
+  return [arr[0], ...capitalizeFirst(arr.slice(1))];
+}
+
+console.log(capitalizeFirst(["car", "taco", "banana"])); // ['Car','Taco','Banana']
