f5ecffce13c228dc76ae66d1d85843a7e4c3fccb
Dada K <dadak@Dadas-MacBook-Air.local>
2022-10-19 12:10:31 +0900

    capitalizeWord

--- Algorithm/recursion(medium).js
+++ Algorithm/recursion(medium).js
@@ -1,18 +1,32 @@
-function reverse(s) {
-  console.log(s);
-  // let str = "";
-  if (s.length <= 1) return s;
-  return reverse(s.slice(1)) + s[0];
-}
+// function reverse(s) {
+//   console.log(s);
+//   // let str = "";
+//   if (s.length <= 1) return s;
+//   return reverse(s.slice(1)) + s[0];
+// }
 
-console.log(reverse("awesome"));
+// console.log(reverse("awesome"));
 
-//capitalize**********************************************************************
-//Zeze's code
-function capitalizeFirst(arr) {
-  if (!arr.length) return [];
-  arr[0] = arr[0].replace(arr[0][0], arr[0][0].toUpperCase());
-  return [arr[0], ...capitalizeFirst(arr.slice(1))];
-}
+// //capitalize**********************************************************************
+// //Zeze's code
+// function capitalizeFirst(arr) {
+//   if (!arr.length) return [];
+//   arr[0] = arr[0].replace(arr[0][0], arr[0][0].toUpperCase());
+//   return [arr[0], ...capitalizeFirst(arr.slice(1))];
+// }
 
-console.log(capitalizeFirst(["car", "taco", "banana"])); // ['Car','Taco','Banana']
+// console.log(capitalizeFirst(["car", "taco", "banana"])); // ['Car','Taco','Banana']
+
+// wordCapitalize**********************************************************************
+
+const capitalizeWords = (arr) => {
+  if (arr.length === 1) {
+    return [arr[0].toUpperCase()];
+  }
+
+  let result = capitalizeWords(arr.slice(0, -1));
+  result.push(arr[arr.length - 1].toUpperCase());
+  return result;
+};
+
+console.log(capitalizeWords(["water", "melon", "sugar"]));
